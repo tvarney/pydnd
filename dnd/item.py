f73e1bd1fec28c95761488a45c3bf4772bf75fb7
tvarney <troy.a.varney@gmail.com>
2018-01-21 00:26:25 -0600

    Added static method from_json to create Item instances from json data

--- dnd/item.py
+++ dnd/item.py
@@ -1,7 +1,7 @@
 
 from typing import TYPE_CHECKING
 if TYPE_CHECKING:
-    from typing import List, Optional, Tuple
+    from typing import Dict, List, Optional, Tuple, Union
 
 
 class Money(object):
@@ -53,21 +53,73 @@ class Money(object):
         return "Money({})".format(", ".join(parts))
 
 
+class Category(object):
+    def __init__(self, name: str):
+        self._name = name  # type: str
+        self._subcategories = dict()  # type: Dict[str, Category]
+        self._items = list()  # type: List[Item]
+
+    def name(self) -> str:
+        return self._name
+
+    def add(self, item: 'Item', category_list: 'List[str]') -> None:
+        category_obj = self  # type: Category
+        for subcategory in category_list:
+            if subcategory not in category_obj._subcategories:
+                category_obj._subcategories[subcategory] = Category(subcategory)
+            category_obj = category_obj._subcategories[subcategory]
+        category_obj._items.append(item)
+
+    def remove(self, item: 'Item', category_list: 'List') -> None:
+        category_obj = self  # type: Category
+        for subcategory in category_list:
+            category_obj = category_obj._subcategories[subcategory]
+        category_obj._items.remove(item)
+
+
 class Item(object):
     All = dict()
-    Category = dict()
+    Category = Category("All")
 
-    Misc = 0
+    Item = 0
     Weapon = 1
     Armor = 2
+    ItemTypeLookup = {"Item": 0, "Weapon": 1, "Armor": 2}
 
-    def __init__(self, key: str, type_: int, name: str, value: 'Money', **kwargs) -> None:
+    @staticmethod
+    def from_json(json_data: 'Dict[str, Union[str, int, float, list, dict]]'):
+        type_str = json_data.pop("type", "Item")
+        type_ = Item.ItemTypeLookup.get(type_str, -1)
+        if type_ == -1:
+            raise KeyError("Invalid Item Type {}".format(type_str))
+
+        if type_ == Item.Weapon:
+            raise NotImplementedError()
+        if type_ == Item.Armor:
+            raise NotImplementedError()
+
+        key = json_data.pop("key", None)
+        if key is None:
+            raise KeyError("Missing required field 'key'")
+
+        name = json_data.pop("name", "")
+        return Item(key, type_, name, **json_data)
+
+    def __init__(self, key: str, type_: int, name: str, **kwargs) -> None:
         self._key = key
         self._type = type_
         self._name = name
-        self._value = value
-        self._desc = str(kwargs.get("description", ""))  # type: str
-        self._categories = list(kwargs.get("categories", list()))  # type: List[str]
+        self._weight = float(kwargs.pop("weight", 0.001))  # type: float
+        self._value = kwargs.pop("value", Money())  # type: Money
+        self._desc = str(kwargs.pop("description", ""))  # type: str
+        self._source = str(kwargs.pop("source", ""))  # type: str
+
+        # These are derived from the key
+        self._categories = self._key.split('.')
+        self._short_key = self._categories.pop()
+        
+        if len(kwargs.keys()) > 0:
+            raise KeyError("Unknown keyword arguments: {}".format(", ".join(kwargs.keys())))
 
     def register(self, replace: bool = False) -> 'Tuple[bool, Optional[str]]':
         err_str = None
@@ -79,8 +131,8 @@ class Item(object):
         Item.All[self._key] = self
         return True, err_str
 
-    def key(self) -> str:
-        return self._key
+    def key(self, full: bool = True) -> str:
+        return self._key if full else self._short_key
 
     def type(self) -> int:
         return self._type
@@ -94,4 +146,5 @@ class Item(object):
     def description(self) -> str:
         return self._desc
 
-    def categories(self) -> str:
+    def categories(self) -> 'List[str]':
+        return self._categories
