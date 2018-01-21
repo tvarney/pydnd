da7d3e21d2e4e023396bb3662f1d129517b97114
tvarney <troy.a.varney@gmail.com>
2018-01-21 02:03:03 -0600

    Moved loading code to a class which aggregates data from the file

--- dnd/io.py
+++ dnd/io.py
@@ -3,17 +3,33 @@ import json
 import dnd.item as _item
 
 
-def load_file(filename: str):
-    data = json.load(open(filename, "rb"))
-    items = data.pop("items", None)
-    if items is not None:
-        if type(items) is not list:
-            print("base-level items collection expected list, got {}".format(type(items)))
-        for index, item_data in enumerate(items):
-            try:
-                item = _item.Item.from_json(item_data)
-                status, err_msg = item.register(False)
-                if not status:
-                    print("Error while registering item {}: {}".format(index, err_msg))
-            except Exception as e:
-                print("{} while parsing item {}: {}".format(type(e), index, str(e)))
+class FileData(object):
+    def __init__(self) -> None:
+        self._log = None
+        self._items = _item.ItemCollection()
+
+    def log(self, message) -> None:
+        if self._log is not None:
+            self._log.append(message)
+        else:
+            print(message)
+
+    def items(self) -> _item.ItemCollection:
+        return self._items
+
+    def load_filename(self, filename: str) -> None:
+        with open(filename, "rb") as fp:
+            data = json.load(fp)
+            items = data.pop("items", None)
+            if items is not None:
+                if type(items) is not list:
+                    self.log("base-level items collection expected list, got {}".format(type(items)))
+                else:
+                    for index, item_data in enumerate(items):
+                        try:
+                            item = _item.Item.from_json(item_data)
+                            status, err_msg = self._items.register(item, False)
+                            if not status:
+                                self.log("Error while registering item {}: {}".format(index, err_msg))
+                        except Exception as e:
+                            self.log("{} while parsing item {}: {}".format(type(e), index, str(e)))
